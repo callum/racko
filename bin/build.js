44e32fe8ffe011964a3fc85ea725e478e0f67eae
Callum <callum@madebymany.co.uk>
2015-04-29 11:19:17 +0100

    Improve builds

--- bin/build.js
+++ bin/build.js
@@ -2,9 +2,14 @@ import fs from 'fs';
 import browserify from 'browserify';
 import babelify from 'babelify';
 import envify from 'envify/custom';
+import uglifyify from 'uglifyify';
 
-browserify('./src/js/index.js')
+const env = process.env.NODE_ENV || 'default';
+
+browserify()
+.add('./src/js/index.js')
 .transform(babelify)
-.transform(envify(require(`./${process.env.NODE_ENV || 'default'}.json`)))
+.transform(envify(require(`../config/${env}.json`)))
+.transform({ global: true }, uglifyify)
 .bundle()
 .pipe(fs.createWriteStream('build/bundle.js'));
--- bin/watch.js
+++ bin/watch.js
@@ -4,13 +4,17 @@ import watchify from 'watchify';
 import babelify from 'babelify';
 import envify from 'envify/custom';
 
-const w = watchify(browserify('./src/js/index.js', { debug: true }));
+const env = process.env.NODE_ENV || 'default';
+const b = browserify(watchify.args);
+const w = watchify(b);
 
 function bundle() {
   w.bundle().pipe(fs.createWriteStream('build/bundle.js'));
 }
 
+w.add('./src/js/index.js');
 w.transform(babelify);
-w.transform(envify(require(`./${process.env.NODE_ENV || 'default'}.json`)));
+w.transform(envify(require(`../config/${env}.json`)));
 w.on('update', bundle);
+w.on('log', (msg) => console.log(msg));
 bundle();

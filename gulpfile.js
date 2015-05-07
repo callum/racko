a0a415658e18437adbdc33a3586c26b391aa00e2
Callum <callum@madebymany.co.uk>
2015-05-07 18:57:43 +0100

    Deploy prep

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 import gulp from 'gulp';
 import autoprefixer from 'gulp-autoprefixer';
-import clean from 'gulp-clean';
 import rename from 'gulp-rename';
 import sass from 'gulp-sass';
 import uglify from 'gulp-uglify';
 import buffer from 'vinyl-buffer';
 import source from 'vinyl-source-stream';
+import del from 'del';
 import browserify from 'browserify';
 import babelify from 'babelify';
 import envify from 'envify/custom';
@@ -18,15 +18,15 @@ try {
   config = require('./config/default.json');
 }
 
-gulp.task('clean', () => {
-  return gulp.src('public', { read: false }).pipe(clean());
+gulp.task('clean', cb => {
+  del(['public'], cb);
 });
 
-gulp.task('build-html', () => {
+gulp.task('build-html', ['clean'], () => {
   return gulp.src('./src/index.html').pipe(gulp.dest('public'));
 });
 
-gulp.task('build-js', () => {
+gulp.task('build-js', ['clean'], () => {
   return browserify('./src/js/index.js')
     .transform(babelify)
     .transform(envify(config))
@@ -37,7 +37,7 @@ gulp.task('build-js', () => {
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('build-scss', () => {
+gulp.task('build-scss', ['clean'], () => {
   return gulp.src('./src/scss/index.scss')
     .pipe(sass({
       outputStyle: 'compressed'
@@ -49,4 +49,4 @@ gulp.task('build-scss', () => {
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('build', ['clean', 'build-html', 'build-js', 'build-scss']);
+gulp.task('build', ['build-html', 'build-js', 'build-scss']);
